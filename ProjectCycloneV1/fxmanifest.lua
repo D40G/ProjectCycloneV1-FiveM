5081f450af8ccb3deaabad272642dedf1a2bdfe4
TheApexDevil <44646723+TheApexDevil@users.noreply.github.com>
2022-02-12 21:40:14 +0100

    Beta Release

---
+++ ProjectCycloneV1/fxmanifest.lua
@@ -0,0 +1,12 @@
+--The sounds are custom made!!
+
+fx_version 'bodacious'
+game 'gta5'
+
+author 'FreakSheep Gamers Network'
+description 'Server Sided Ambiance'
+version '1.0.0'
+
+files { 
+	"stream/weather.awc"
+}
